289455936e3d8d16953aabc6894e455d95f118d7
Chahoud Soufiane <soufi@10.117.66.30>
2016-02-25 18:21:06 +0100

    Initial commit of the project

---
+++ ProjetWeb/src/main/java/fr/uha/miage/ProjetWebApplication.java
@@ -0,0 +1,12 @@
+package fr.uha.miage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetWebApplication.class, args);
+	}
+}
